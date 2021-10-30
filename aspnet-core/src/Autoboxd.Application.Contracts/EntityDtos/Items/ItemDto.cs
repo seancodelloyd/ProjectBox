751163597d84aa52f584a107afc02222dd8d83e9
Sean Lloyd <sean.lloydbooth@teamdatalogic.com>
2021-10-30 13:19:28 +0100

    Create DTO for item and map

---
+++ aspnet-core/src/Autoboxd.Application.Contracts/EntityDtos/Items/ItemDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Autoboxd.Items
+{
+    public class ItemDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ManufacturedYear { get; set; }
+    }
+}
--- aspnet-core/src/Autoboxd.Application/AutoboxdApplicationAutoMapperProfile.cs
+++ aspnet-core/src/Autoboxd.Application/AutoboxdApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Autoboxd.Items;
+using AutoMapper;
 
 namespace Autoboxd
 {
@@ -6,9 +7,7 @@ namespace Autoboxd
     {
         public AutoboxdApplicationAutoMapperProfile()
         {
-            /* You can configure your AutoMapper mapping configuration here.
-             * Alternatively, you can split your mapping configurations
-             * into multiple profile classes for a better organization. */
+            CreateMap<Item, ItemDto>();
         }
     }
 }
