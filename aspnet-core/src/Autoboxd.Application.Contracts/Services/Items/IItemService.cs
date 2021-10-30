263f353f3fc5529a556fc735be72d0fc5e8a97d4
Sean Lloyd <sean.lloydbooth@teamdatalogic.com>
2021-10-30 13:49:21 +0100

    Added item service

---
+++ aspnet-core/src/Autoboxd.Application.Contracts/Services/Items/IItemService.cs
@@ -0,0 +1,15 @@
+﻿using Autoboxd.Items;
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Autoboxd.Services.Items
+{
+    public interface IItemService : ICrudAppService<
+        ItemDto,
+        Guid,
+        PagedAndSortedResultRequestDto,
+        CreateUpdateItemDto>
+    {
+    }
+}
---
+++ aspnet-core/src/Autoboxd.Application/Services/Items/ItemService.cs
@@ -0,0 +1,22 @@
+﻿using Autoboxd.Items;
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Autoboxd.Services.Items
+{
+    public class ItemService : CrudAppService<
+            Item, //The Book entity
+            ItemDto, //Used to show books
+            Guid, //Primary key of the book entity
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateItemDto>, //Used to create/update a book
+            IItemService
+    {
+        public ItemService(IRepository<Item, Guid> repository) : base(repository)
+        {
+
+        }
+    }
+}
