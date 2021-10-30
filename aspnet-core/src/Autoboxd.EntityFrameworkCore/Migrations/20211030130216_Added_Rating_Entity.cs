89595ec9c1a89c8c285b96ab1509f691d5f9f868
Sean Lloyd <sean.lloydbooth@teamdatalogic.com>
2021-10-30 14:03:43 +0100

    Rating EF entity migrations

---
+++ aspnet-core/src/Autoboxd.EntityFrameworkCore/Migrations/20211030130216_Added_Rating_Entity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Autoboxd.Migrations
+{
+    public partial class Added_Rating_Entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppRatings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppRatings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppRatings_AppItems_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "AppItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppRatings_ItemId",
+                table: "AppRatings",
+                column: "ItemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppRatings");
+        }
+    }
+}
